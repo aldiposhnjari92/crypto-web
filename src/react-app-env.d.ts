7fd1cf0737095011d75405dc2747453e73fc00f7
Aldi Poshnjari <aldiposhnjari@aldi.local>
2022-07-09 23:06:58 +0200

    new changes

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="react-scripts" />
+
+apiEndPoint = "https://api.multifarm.fi/jay_flamingo_random_6ix_vegas/get_assets?pg=1&tvl_min=50000&sort=tvlStaked&sort_order=desc&farms_tvl_staked_gte=10000000"
